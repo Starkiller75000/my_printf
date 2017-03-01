89931a9c2e8318c837a2fd12b5e17d62b6711458
Starkiller75000 <darkmol83@hotmail.fr>
2017-03-01 10:25:21 +0100

    Update my_put_nbr.c

--- my_put_nbr.c
+++ my_put_nbr.c
@@ -1,11 +1,11 @@
 /*
 ** my_put_nbr.c for my_put_nbr in /home/Starkiller/CPool_Day03
 ** 
-** Made by Bouton Benoit
+** Made by starkiller
 ** Login   <Starkiller@epitech.net>
 ** 
-** Started on  Thu Oct  6 17:20:26 2016 Bouton Benoit
-** Last update Sun Oct  9 16:52:01 2016 Bouton Benoit
+** Started on  Thu Oct  6 17:20:26 2016 starkiller
+** Last update Sun Oct  9 16:52:01 2016 starkiller
 */
 
 int	 my_put_nbr(int nb)
