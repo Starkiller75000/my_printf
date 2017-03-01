0158dfb2c5c5dd908e50681176b64740d858234f
Starkiller75000 <darkmol83@hotmail.fr>
2017-03-01 10:26:46 +0100

    Update my_putstr.c

--- my_putstr.c
+++ my_putstr.c
@@ -1,11 +1,11 @@
 /*
 ** my_putstr.c for my_putstr in /home/Starkiller/CPool_Day04
 ** 
-** Made by Bouton Benoit
+** Made by starkiller
 ** Login   <Starkiller@epitech.net>
 ** 
-** Started on  Thu Oct  6 14:10:45 2016 Bouton Benoit
-** Last update Tue Oct 18 13:21:41 2016 Bouton Benoit
+** Started on  Thu Oct  6 14:10:45 2016 starkiller
+** Last update Tue Oct 18 13:21:41 2016 starkiller
 */
 
 int	 my_putstr(char *str)
