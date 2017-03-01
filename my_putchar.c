76861144002c124a4ceb5bf852d493cf5b3b8c8d
Starkiller75000 <darkmol83@hotmail.fr>
2017-03-01 10:25:56 +0100

    Update my_putchar.c

--- my_putchar.c
+++ my_putchar.c
@@ -1,11 +1,11 @@
 /*
 ** my_putchar.c for my_putchar in /home/Starkiller/LIB
 ** 
-** Made by Bouton Benoit
+** Made by starkiller
 ** Login   <Starkiller@epitech.net>
 ** 
-** Started on  Tue Oct 18 15:18:14 2016 Bouton Benoit
-** Last update Tue Oct 18 15:18:56 2016 Bouton Benoit
+** Started on  Tue Oct 18 15:18:14 2016 starkiller
+** Last update Tue Oct 18 15:18:56 2016 starkiller
 */
 
 void	my_putchar(char c)
