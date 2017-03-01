c5e2d98a3b533629540c370dddcaf0282f36092b
Starkiller75000 <darkmol83@hotmail.fr>
2017-03-01 10:27:24 +0100

    Update my_strlen.c

--- my_strlen.c
+++ my_strlen.c
@@ -1,11 +1,11 @@
 /*
 ** my_strlen.c for my_strlen in /home/Starkiller/CPool_Day04
 ** 
-** Made by Bouton Benoit
+** Made by starkiller
 ** Login   <Starkiller@epitech.net>
 ** 
-** Started on  Thu Oct  6 16:41:47 2016 Bouton Benoit
-** Last update Thu Oct 27 14:58:15 2016 Bouton Benoit
+** Started on  Thu Oct  6 16:41:47 2016 starkiller
+** Last update Thu Oct 27 14:58:15 2016 starkiller
 */
 
 int	 my_strlen(char *str)
